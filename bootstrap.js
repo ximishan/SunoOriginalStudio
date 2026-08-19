723150e865250ade0b72ac22b41bcd6c985a7527
zhangfeng <908980328@qq.com>
2026-08-19 16:07:00 +0800

    Register persistent song library IPC

--- bootstrap.js
+++ bootstrap.js
@@ -1,8 +1,10 @@
 const { app, ipcMain, dialog, shell } = require('electron');
 const { registerDeaiIpc } = require('./deai');
+const { registerSongLibraryIpc } = require('./song_library');
 
 require('./main');
 
 app.whenReady().then(() => {
   registerDeaiIpc({ app, ipcMain, dialog, shell });
+  registerSongLibraryIpc({ app, ipcMain, dialog, shell });
 });
