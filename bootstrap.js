fcfc6c30df603e7b6de795697bb32170f77d7f55
zhangfeng <908980328@qq.com>
2026-08-19 15:23:09 +0800

    Pass app to exact N19 engine

--- bootstrap.js
+++ bootstrap.js
@@ -1,9 +1,8 @@
 const { app, ipcMain, dialog, shell } = require('electron');
 const { registerDeaiIpc } = require('./deai');
 
-// Keep the existing Suno original-generation main process intact.
 require('./main');
 
 app.whenReady().then(() => {
-  registerDeaiIpc({ ipcMain, dialog, shell });
+  registerDeaiIpc({ app, ipcMain, dialog, shell });
 });
