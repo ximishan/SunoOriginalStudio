455aed3e6be025dfe197a01cc06a6706139a8582
xishan <908980328@qq.com>
2026-09-06 18:42:11 +0800

    feat: install reference EXE download strategy before transport fallback

--- bootstrap.js
+++ bootstrap.js
@@ -169,6 +169,7 @@ async function warmUnknownAccountStates() {
 const profileInfo = prepareStableProfile();
 const { installSongLibraryWriteGuard } = require('./song_library_guard');
 const { installAuthFetchGuard } = require('./auth_fetch_guard');
+const { installReferenceDownloadCompatibility } = require('./reference_download_compat');
 const { installSunoTransportCompatibility } = require('./suno_transport_compat');
 const { installMatureMediaFetch } = require('./mature_media_fetch');
 installSongLibraryWriteGuard(app);
@@ -181,6 +182,7 @@ require('./main');
 
 app.whenReady().then(() => {
   installAuthFetchGuard();
+  installReferenceDownloadCompatibility();
   installSunoTransportCompatibility();
   installMatureMediaFetch();
   installAccountPersistence();
